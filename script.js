a6284854332dda1d1a8101968ceca33212fb2ced
Abilashkumar06 <abilashkumar730@gmail.com>
2026-08-22 18:37:17 +0530

    initial portfolio

---
+++ script.js
@@ -0,0 +1,34 @@
+// Simple welcome message
+console.log("Welcome to Abilash P's Portfolio!");
+
+// Highlight navigation link while scrolling
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 150;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop &&
+            scrollY < sectionTop + sectionHeight) {
+
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
